3f33175a809f8719b6719d716717a0d3e05af4fe
Mauro <mauro.valerioti.18@gmail.com>
2024-01-16 15:51:03 -0300

    se agrego modificaciones en el controller

--- Java/product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
+++ Java/product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.wannacode.productmicroservice.entity.ProductEntity;
 import com.wannacode.productmicroservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +20,17 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public List<ProductEntity> getAllProducts(){
         return productRepository.findAll();
+
     }
+
+//  ESTE ES UN EJEMPLO DE COMO SE HARIA LA RESPUESTA HTTP EN CASO DE NO QUERER USAR EL @ResponseStatus
+//    @GetMapping
+//    public ResponseEntity<List<ProductEntity>> getAllProducts(){
+//    List<ProductEntity> productEntities = productRepository.findAll();
+//    ResponseEntity<List<ProductEntity>> responseEntity = new ResponseEntity<>(productEntities, HttpStatus.OK);
+//
+//    return responseEntity;
+//    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)// esto sirve para que spring envie un mensaje de OK . facilita mucho la respuesta Http
