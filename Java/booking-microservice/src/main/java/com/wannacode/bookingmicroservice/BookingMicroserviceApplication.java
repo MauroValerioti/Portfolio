c1ff6b86341a201d4577475d7fc8c3aadacf68b0
Mauro <mauro.valerioti.18@gmail.com>
2024-01-16 16:09:49 -0300

    se agrego nuevo microservicio de booking al repo

---
+++ Java/booking-microservice/src/main/java/com/wannacode/bookingmicroservice/BookingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wannacode.bookingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMicroserviceApplication.class, args);
+	}
+
+}
