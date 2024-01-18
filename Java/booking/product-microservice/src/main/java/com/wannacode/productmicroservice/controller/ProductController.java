46ca80f9ba2e372e70be81891f4fc3fe5fc19448
Mauro <mauro.valerioti.18@gmail.com>
2024-01-18 10:42:55 -0300

    se modificaron los folders

--- Java/booking/product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
+++ Java/booking/product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
@@ -4,7 +4,7 @@ import com.wannacode.productmicroservice.entity.ProductEntity;
 import com.wannacode.productmicroservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
