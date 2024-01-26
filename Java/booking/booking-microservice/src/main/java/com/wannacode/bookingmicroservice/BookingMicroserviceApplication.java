cee4e8c17d0417f29426e9b5e3e672db5a7c50e8
Mauro <mauro.valerioti.18@gmail.com>
2024-01-26 20:42:51 -0300

    se agregaron cambios al repo

--- Java/booking/booking-microservice/src/main/java/com/wannacode/bookingmicroservice/BookingMicroserviceApplication.java
+++ Java/booking/booking-microservice/src/main/java/com/wannacode/bookingmicroservice/BookingMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.wannacode.bookingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
